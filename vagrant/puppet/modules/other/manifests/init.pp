a32b6f55b7403ea6e27463e6e0422d7764ec2d74
Anthony Hastings (Laptop) <anthony@anthonyhastings.com>
2014-01-22 15:05:09 +0000

    - Better laying out multiple module dependencies into separate arrays. Also adding cURL to the other modules array.

--- vagrant/puppet/modules/other/manifests/init.pp
+++ vagrant/puppet/modules/other/manifests/init.pp
@@ -1,15 +1,19 @@
 class other {
-  
-  package { [ 'vim',
-              'lynx']:
-    ensure => present;
-  }
 
-  file { 
-    '/etc/motd':
-      ensure  => present,
-      replace => true,
-      source  => 'puppet:///modules/other/welcome.txt';
-  }
+    $modules = [
+        'curl',
+        'vim',
+        'lynx'
+    ]
+
+    package { $modules:
+        ensure => present;
+    }
+
+    file { '/etc/motd':
+        ensure  => present,
+        replace => true,
+        source  => 'puppet:///modules/other/welcome.txt';
+    }
 
 }
--- vagrant/puppet/modules/php/manifests/init.pp
+++ vagrant/puppet/modules/php/manifests/init.pp
@@ -1,22 +1,34 @@
 class php {
 
-  package { ['php5',
-             'php5-cli',
-             'php5-fpm',
-             'php-apc',
-             'php5-curl',
-             'php5-dev',
-             'php5-gd',
-             'php5-imagick',
-             'php5-mcrypt',
-             'php5-memcache',
-             'php5-mysql',
-             'php5-pspell',
-             'php5-tidy',
-             'php5-xdebug',
-             'php5-xmlrpc',
-             'php5-xsl']:
-    ensure => present;
-  }
+    $modules = [
+        'php5',
+        'php5-cli',
+        'php5-curl',
+        'php5-dev',
+        'php5-fpm',
+        'php5-gd',
+        'php5-imagick',
+        'php5-mcrypt',
+        'php5-mysql',
+        'php5-pspell',
+        'php5-tidy',
+        'php5-xdebug',
+        'php5-xmlrpc',
+        'php5-xsl',
+        'php-pear'
+    ]
+
+    package { $modules:
+        ensure => present;
+    }
+
+    exec { 'phpunit':
+        command => "pear upgrade PEAR && \
+                    pear config-set auto_discover 1 && \
+                    pear install --alldeps pear.phpunit.de/PHPUnit",
+        creates => '/usr/bin/phpunit',
+        path    => ['/bin', '/usr/bin'],
+        require => Package['php-pear'];
+    }
 
 }
