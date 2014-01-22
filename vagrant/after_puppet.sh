f6fa1eb35dec65d9831636348a226391672ba53e
Anthony Hastings (Laptop) <anthony@anthonyhastings.com>
2014-01-22 16:58:29 +0000

    - Port forarding fixed for MySQL.

---
+++ vagrant/after_puppet.sh
@@ -0,0 +1,1 @@
+echo -e "\e[1;33m[REHAB-BOX]: After Puppet Shell. Executing.\e[0m";
--- vagrant/before_puppet.sh
+++ vagrant/before_puppet.sh
@@ -1,1 +1,3 @@
+echo -e "\e[1;33m[REHAB-BOX]: Before Puppet Shell. Executing.\e[0m";
+
 sudo apt-get update --fix-missing
