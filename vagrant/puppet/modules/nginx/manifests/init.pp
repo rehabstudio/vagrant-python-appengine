365ea15ba8b06cddfb3778f213fabbee6eb1da3d
Anthony Hastings (Laptop) <anthony@anthonyhastings.com>
2014-01-23 09:34:16 +0000

    - Reformatting nginx module since editorconfig is now present, and also adding www-data user to the vagrant group.

--- vagrant/puppet/modules/nginx/manifests/init.pp
+++ vagrant/puppet/modules/nginx/manifests/init.pp
@@ -1,32 +1,38 @@
 class nginx {
-  
-  package { 'nginx':
-    ensure => present;
-  }
 
-  service { 'nginx':
-    ensure  => running,
-    require => Package['nginx'];
-  }
+    package { 'nginx':
+        ensure => present;
+    }
 
-  file {
-    '/etc/nginx/nginx.conf':
-      ensure  => present,
-      source  => 'puppet:///modules/nginx/nginx.conf',
-      require => Package['nginx'],
-      notify  => Service['nginx'];
+    service { 'nginx':
+        ensure  => running,
+        require => Package['nginx'];
+    }
 
-    '/etc/nginx/proxy_params':
-      ensure  => present,
-      source  => 'puppet:///modules/nginx/proxy_params',
-      require => Package['nginx'],
-      notify  => Service['nginx'];
+    file {
+        '/etc/nginx/nginx.conf':
+            ensure  => present,
+            source  => 'puppet:///modules/nginx/nginx.conf',
+            require => Package['nginx'],
+            notify  => Service['nginx'];
 
-    '/etc/nginx/sites-enabled/default':
-      ensure  => present,
-      replace => true,
-      source  => 'puppet:///modules/nginx/default',
-      require => Package['nginx'],
-      notify  => Service['nginx'];
-  }
+        '/etc/nginx/proxy_params':
+            ensure  => present,
+            source  => 'puppet:///modules/nginx/proxy_params',
+            require => Package['nginx'],
+            notify  => Service['nginx'];
+
+        '/etc/nginx/sites-enabled/default':
+            ensure  => present,
+            replace => true,
+            source  => 'puppet:///modules/nginx/default',
+            require => Package['nginx'],
+            notify  => Service['nginx'];
+    }
+
+    user { 'www-data':
+        groups => ['vagrant'],
+        notify => Service['nginx']
+    }
+
 }
