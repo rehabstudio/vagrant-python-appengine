d5eaf4fd90f4dfbf53a00938a9b892efc7f6de6a
Anthony Hastings (Laptop) <anthony@anthonyhastings.com>
2014-01-23 08:50:11 +0000

    - Adding global Exec statement.

--- vagrant/puppet/manifests/init.pp
+++ vagrant/puppet/manifests/init.pp
@@ -1,3 +1,8 @@
+# Adding a global exec statement so we don't have to add paths to every one.
+Exec {
+    path => ['/bin', '/sbin', '/usr/bin', '/usr/sbin']
+}
+
 include nginx
 include php
 include mysql
