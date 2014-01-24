970c63c722d6e262b778ea101f69892a88584a4d
Anthony Hastings <anthony@rehabstudio.com>
2014-01-24 17:52:32 +0000

    - Adding PHP logic using the PHP module as a base.

--- vagrant/puppet/manifests/init.pp
+++ vagrant/puppet/manifests/init.pp
@@ -45,6 +45,54 @@ mysql_grant { "${databaseUser}@%/${databaseName}":
     require => Class['mysql::server']
 }
 
-include other
-include nginx
+# Including necessary classes for installing PHP.
 include php
+include php::composer
+include php::fpm::daemon
+
+# Installing FPM and PEAR separately as they have extra functionality.
+class {
+    'php::fpm':
+        ensure => installed,
+        settings => {
+            set => {
+            }
+        };
+    'php::pear':
+        ensure => installed;
+}
+
+# Installing PHP Extensions.
+php::extension { 'php-extensions':
+    ensure => installed,
+    package => ['php5-cli', 'php5-curl', 'php5-dev',
+                'php5-gd', 'php5-imagick', 'php5-mcrypt',
+                'php5-mysql', 'php5-pspell', 'php5-xdebug',
+                'php5-xmlrpc', 'php5-tidy', 'php5-xsl'];
+}
+
+# Installing PHPUnit via Pear.
+package { 'pear.phpunit.de/PHPUnit':
+    ensure   => present,
+    provider => pear,
+    require  => [Package['php-pear'], Exec['php::pear::auto_discover']];
+}
+
+# Updating particular settings in the ini file without doing a file overwrite.
+augeas { 'php.ini':
+    notify => Service['php5-fpm'],
+    require => [Package['libaugeas-ruby'], Package['augeas-tools'], Package['php5-fpm']],
+    context => '/files/etc/php5/fpm/php.ini',
+    changes => [
+        "set PHP/short_open_tag Off",
+        "set PHP/expose_php Off",
+        "set PHP/display_errors On",
+        "set PHP/html_errors On",
+        "set PHP/post_max_size 128M",
+        "set PHP/upload_max_filesize 128M",
+        "set Date/date.timezone Europe/Belfast"
+    ]
+}
+
+include nginx
+include other
