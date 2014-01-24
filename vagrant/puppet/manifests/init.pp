4001f46db4b0a4a317f0e80dcc0c1851a8fb163e
Anthony Hastings <anthony@rehabstudio.com>
2014-01-24 12:19:36 +0000

    - Adding MySQL module settings.

--- vagrant/puppet/manifests/init.pp
+++ vagrant/puppet/manifests/init.pp
@@ -1,9 +1,27 @@
+# Project specific settings.
+$databaseName = 'your-database-name'
+$databaseUser = 'projectuser'
+$databasePass = '6LG621D15l37Yzv'
+$databaseFile = '/vagrant/schema/db.sql'
+
 # Adding a global exec statement so we don't have to add paths to every one.
 Exec {
     path => ['/bin', '/sbin', '/usr/bin', '/usr/sbin']
 }
 
+# Installing MySQL server and updating the root users password.
+class { '::mysql::server':
+    root_password => 'root';
+}
+
+# Adding project database schema to the MySQL database.
+mysql::db { $databaseName:
+    grant    => ['ALL'],
+    user     => $databaseUser,
+    password => $databasePass,
+    sql      => $databaseFile
+}
+
 include other
 include nginx
 include php
-include mysql
