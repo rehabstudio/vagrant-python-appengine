f854df86d00969ad96c102853948b3fdbdfb2bda
Paddy Carey <patrick@rehabstudio.com>
2014-02-18 20:20:28 +0000

    added a shell provisioner that bootstraps a minimal env (enough for librarian-puppet to install any required modules)

--- vagrant/before_puppet.sh
+++ vagrant/before_puppet.sh
@@ -1,1 +1,18 @@
 echo -e "\e[1;33m[REHAB-BOX]: Before Puppet Shell. Executing.\e[0m";
+
+
+# fetch the puppet release deb for precise
+wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb
+# install the deb once downloaded
+sudo dpkg -i puppetlabs-release-precise.deb
+
+# install required packages
+sudo apt-get -qq update --fix-missing
+sudo apt-get install git puppet --assume-yes
+
+# install librarian-puppet gem from rubygems
+sudo gem install librarian-puppet
+
+# install all module dependencies
+cd /vagrant/puppet/
+librarian-puppet install
