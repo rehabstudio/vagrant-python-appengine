df9cb71fa712566738104d9ca6b83c849103427b
Paddy Carey <patrick@rehabstudio.com>
2014-02-18 23:13:49 +0000

    updated before_puppet.sh to make a bit more robust

--- vagrant/before_puppet.sh
+++ vagrant/before_puppet.sh
@@ -1,18 +1,39 @@
+#!/bin/bash
 echo -e "\e[1;33m[REHAB-BOX]: Before Puppet Shell. Executing.\e[0m";
 
 
-# fetch the puppet release deb for precise
-wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb
-# install the deb once downloaded
-sudo dpkg -i puppetlabs-release-precise.deb
+if [ ! -f /root/puppetlabs-release-precise.deb ]; then
+    echo 'Installing Puppet apt repository'
+    cd /root/
+    # fetch the puppet release deb for precise
+    wget --quiet https://apt.puppetlabs.com/puppetlabs-release-precise.deb
+    # install the deb once downloaded
+    dpkg -i puppetlabs-release-precise.deb
+fi
+
+
+if [ ! -f /etc/puppet/hiera.yaml ]; then
+    echo 'Ensuring hiera configuration file exists'
+    # Hiera is a key/value lookup tool for configuration data. We don't
+    # actually use it but puppet'll throw an error if this file doesn't exist
+    # at provision time
+    touch /etc/puppet/hiera.yaml
+fi
+
 
 # install required packages
+echo "Updating/Installing required packages"
 sudo apt-get -qq update --fix-missing
-sudo apt-get install git puppet --assume-yes
+sudo apt-get -qq install git puppet --assume-yes
 
-# install librarian-puppet gem from rubygems
-sudo gem install librarian-puppet
 
-# install all module dependencies
+if [ -z `which librarian-puppet` ]; then
+    echo "Installing librarian-puppet from rubygems"
+    # install librarian-puppet gem from rubygems
+    sudo gem install librarian-puppet
+fi
+
+# install/update puppet module dependencies
+echo "Installing puppet module dependencies with librarian-puppet"
 cd /vagrant/puppet/
 librarian-puppet install
