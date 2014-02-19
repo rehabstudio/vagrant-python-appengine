6388f626577006d46c1eeb766242be96542dd550
Paddy Carey <patrick@rehabstudio.com>
2014-02-19 23:09:58 +0000

    added builddeps for PIL and mysqldb, and cleaned up appengine manifest

--- vagrant/puppet/manifests/appengine.pp
+++ vagrant/puppet/manifests/appengine.pp
@@ -1,10 +1,25 @@
 # install the appengine sdk inside /opt
 archive { 'google_appengine_sdk':
-   ensure => present,
-   url => 'http://googleappengine.googlecode.com/files/google_appengine_1.8.9.zip',
-   target => '/opt',
-   follow_redirects => true,
-   extension => 'zip',
-   checksum => false,
-   src_target => '/tmp'
+    ensure => present,
+    url => 'http://googleappengine.googlecode.com/files/google_appengine_1.8.9.zip',
+    target => '/opt',
+    follow_redirects => true,
+    extension => 'zip',
+    checksum => false,
+    src_target => '/tmp',
+    require => Package['unzip']
+}
+
+# ensure the sdk install directory is on the system path
+file_line { 'appengine path':
+    ensure => present,
+    path   => '/etc/bash.bashrc',
+    line   => 'export PATH="$PATH:/opt/google_appengine"',
+}
+
+# add a handy alias so users can run the sdk with sensible defaults
+file_line { 'appengine alias':
+    ensure => present,
+    path   => '/etc/bash.bashrc',
+    line   => 'alias runserver="dev_appserver.py /home/vagrant/app --host 0.0.0.0 --admin_host 0.0.0.0 --storage_path /home/vagrant/storage --skip_sdk_update_check"',
 }
--- vagrant/puppet/manifests/apt_packages.pp
+++ vagrant/puppet/manifests/apt_packages.pp
@@ -2,20 +2,34 @@
 class { 'apt': }
 apt::ppa { 'ppa:git-core/ppa': }
 
-
 # Installing other useful packages.
 package { [
-    'curl',
+    # latest stable git
+    'git-core',
+    # vim 'cos vim
+    'vim',
+    # in-memory cache
+    'memcached',
+    # because we need to uncompress things
     'unzip',
     'zip',
-    'git-core',
-    'vim',
-    'memcached',
+    # python development dependencies
+    # we only install a minimal set of packages, anything else can be
+    # installed inside a virtualenv as required on the box
     'python-dev',
-    'python-imaging',
     'python-virtualenv',
     'virtualenvwrapper',
+    # we'll make an exception for PIL and install it globally from apt because
+    # it's the simplest way to make it work nicely with the appengine sdk
+    # NOTE: this is futile in versions of ubuntu after 12.04, in that case
+    # install Pillow in a virtualenv instead.
+    'python-imaging',
 ]:
     ensure => present,
     require => Apt::Ppa['ppa:git-core/ppa']
 }
+
+# install the build dependencies for both PIL and mysqldb, this allows both to
+# be easily built inside a virtualenv when required
+apt::builddep { 'python-imaging': }
+apt::builddep { 'python-mysqldb': }
